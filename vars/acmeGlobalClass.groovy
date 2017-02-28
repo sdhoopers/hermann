17c65b86fa19c39eefa5ec35c35e8f4089936302
Liam Newman <bitwiseman@gmail.com>
2017-02-28 14:11:23 -0800

    Continue testing

--- vars/acmeGlobalClass.groovy
+++ vars/acmeGlobalClass.groovy
@@ -1,16 +1,10 @@
-class Acme implements Serializable {
-    private final Script script
-    private String name = 'human'
-    Acme(Script script) {
-        this.script = script
-    }
-    def setName(value) {
-        name = value
-    }
-    def getName() {
-        name
-    }
-    def caution(message) {
-        script.echo "Hello, ${name}! CAUTION: ${message}"
-    }
+// vars/acme.groovy
+def setName(value) {
+    namePrivate = value
+}
+def getName() {
+    namePrivate
+}
+def caution(message) {
+    echo "Hello, ${namePrivate}! CAUTION: ${message}"
 }
