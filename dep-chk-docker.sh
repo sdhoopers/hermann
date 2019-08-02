4eb8c96e45bbb0625f8f6e40655c423812bbf26b
Shawn Hooper <shawn.hooper@eccalon.com>
2019-08-02 11:10:32 -0400

    change data dir

--- dep-chk-docker.sh
+++ dep-chk-docker.sh
@@ -18,10 +18,10 @@ docker pull owasp/dependency-check
 
 docker run --rm \
     --volume $(pwd):/src \
-    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
+    --volume "$DATA_DIRECTORY":dependency-check-data \
     --volume "$REPORT_DIRECTORY":/report \
     owasp/dependency-check \
-    --scan /src \
+    --scan /src     \
     --format "ALL" \
     --project "My OWASP Dependency Check Project" \
     --out /report
